efd34f6b52fc551ed3d956d36732dc904b4ecb62
hero-BA <suad.hero@gmail.com>
2021-03-30 10:14:12 +0200

    Added exercise: Create a Box Class Component

--- ex04/src/Box.js
+++ ex04/src/Box.js
@@ -1,10 +1,8 @@
-import React, { Component } from "react";
+import React, { Component } from 'react';
 
 class Box extends Component {
   render() {
-    return (
-        <div>This is a Box Component!</div>
-    )
+    return <div>This is a Box Component!</div>;
   }
 }
 
